fb4681b2ed5949e0f04e95d61526c62596ea0f19
Michael Pacheco <git@michaelpacheco.org>
2023-01-17 23:06:34 -0800

    dfa: Optmizations.
    * I am not storing any strings other than the raw one anymore. This means instead of an possibly large allocation of owned Strings, Now a start and stop locations are stored.
    * There are two new functions.
        * print_result only allocates when needed and prints the quote from the locations from the slice vector.
        * return_results returns an iterator of Strings that are ownable, and thus can be used outside the struct if wanted.
    
    Signed-off-by: Michael Pacheco <git@michaelpacheco.org>

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,11 +1,11 @@
+#![allow(clippy::missing_docs_in_private_items)]
 use conversions_rust_lib::ToUnicodeVec;
 use std::str;
 
 #[derive(Debug)]
 struct CollectQoutes<'a> {
     state: State,
-    quotes: Vec<String>,
-
+    slices: Vec<(usize, usize)>,
     raw_str: Vec<&'a str>,
 }
 
@@ -20,11 +20,11 @@ impl<'a> CollectQoutes<'a> {
     fn new(raw_str: &'a str) -> Self {
         Self {
             state: State::None,
-            quotes: vec![],
+            slices: vec![],
             raw_str: raw_str.to_characters().collect::<Vec<&'a str>>(),
         }
     }
-    fn process(&mut self) -> Result<(), liberr::Err> {
+    fn process(&mut self) {
         let mut quote_index = 0;
         for (i, &str_char) in self.raw_str.iter().enumerate() {
             if str_char == "\"" {
@@ -34,14 +34,21 @@ impl<'a> CollectQoutes<'a> {
                         State::LeftHandQuote
                     }
                     State::LeftHandQuote => {
-                        self.quotes
-                            .push(String::from(&self.raw_str[quote_index..=i].join("")));
+                        self.slices.push((quote_index, i));
                         State::RightHandQuote
                     }
                 }
             }
         }
-        Ok(())
+    }
+    fn print_results(&self) {
+        for (i, &(start, end)) in self.slices.iter().enumerate() {
+            let slice = &self.raw_str[start..=end].join("");
+            println!("QUOTE #{} of {}: {slice}", i+1, self.slices.len());
+        }
+    }
+    fn return_results(&self) -> impl Iterator<Item = String> + '_ {
+        self.slices.iter().map(|&(start, end)|String::from(&self.raw_str[start..=end].join("")))
     }
 }
 
@@ -50,11 +57,13 @@ mod tests {
     use super::*;
 
     #[test]
-    fn detects_left_quote() -> Result<(), liberr::Err> {
+    fn detects_left_quote() {
         let test_str = "Marcus said, \"Yo! Have you eaten? \". I replied, \"Not yet. I am currently looking for food now. How about you?\"";
-        let mut dfa = CollectQoutes::new(test_str);
-        dfa.process()?;
-        println!("{:?}", dfa);
-        Ok(())
+        let mut quote_collector = CollectQoutes::new(test_str);
+        quote_collector.process();
+        println!("{quote_collector:?}" );
+        quote_collector.print_results();
+        let results = quote_collector.return_results().collect::<Vec<String>>();
+        println!("{results:?}");
     }
 }
